d0c19bf5ec2b3601990ee2214aa9ea106e4bf4b6
archae0pteryx <aarchaeopteryxx@gmail.com>
2022-07-01 09:46:50 -0700

    fix stopped check

--- src/auto-shutdown/index.js
+++ src/auto-shutdown/index.js
@@ -1,5 +1,7 @@
 const AWS = require('aws-sdk')
+
 const ec2 = new AWS.EC2()
+const hasOnlinePlayer = require('./hasOnlinePlayer')
 
 const { INSTANCE_ID, SHUTDOWN_AFTER_MINS } = process.env
 
@@ -7,26 +9,42 @@ const EC2_PARAMS = {
   InstanceIds: [INSTANCE_ID],
 }
 
-exports.handler = async (event) => {
+function _shutdownInstance() {
+  return ec2
+    .stopInstances({
+      InstanceIds: [INSTANCE_ID],
+    })
+    .promise()
+}
+
+function _hasTimeExpired(launchTime) {
   const now = Date.now()
+  const serverUpForMins = Math.floor((now - launchTime) / 1000 / 60)
+  return serverUpForMins >= SHUTDOWN_AFTER_MINS
+}
+
+function _shouldServerShutdown(launchTime) {
+  return _hasTimeExpired(launchTime) && !hasOnlinePlayer()
+}
+
+exports.handler = async (event) => {
+  // const now = Date.now()
   const data = await ec2.describeInstances(EC2_PARAMS).promise()
   const instance = data.Reservations?.[0].Instances?.[0]
   if (!instance) {
     throw new Error('Autoshutdown cant find instance')
   }
   const launchTime = Date.parse(instance.LaunchTime)
-  const serverUpForMins = Math.floor((now - launchTime) / 1000 / 60)
-  if (serverUpForMins >= SHUTDOWN_AFTER_MINS) {
-    await ec2
-      .stopInstances({
-        InstanceIds: [INSTANCE_ID],
-      })
-      .promise()
-    console.log(`shutdown: ${INSTANCE_ID}`)
-    return
+  const shouldShutdown = _shouldServerShutdown(instance.LaunchTime)
+  if (shouldShutdown) {
+    const res = await _shutdownInstance()
+    return {
+      statusCode: 200,
+      body: res,
+    }
   }
-  console.log(
-    `Server not shutdown. uptime: ${serverUpForMins} minutes. Shutdown after: ${SHUTDOWN_AFTER}`
-  )
-  return
+  return {
+    statusCode: 200,
+    body: 'Should not shutdown server',
+  }
 }
